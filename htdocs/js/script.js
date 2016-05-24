411872fe1c39cae5bce09055ab5eb5615cfbab35
Dave Sayer <dave@bathdesign.co.uk>
2016-05-24 14:15:05 +0100

    no need for scrollspy at the mo

--- htdocs/js/script.js
+++ htdocs/js/script.js
@@ -5,47 +5,47 @@ jQuery( document ).ready( function($) {
 
     $(function() {
 
-            var $object = $('.header');
-            var position = $(this).position();
-            var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
-            $object.scrollspy({
-                min: 0,
-                max: 200,
-                onEnter: function(element, position) {
-                    console.log('Logo');
-                    if(!$object.hasClass('fadeIn')){
-                        $object.addClass('hinge').one(animationEnd, function() {
-                            $(this).removeClass('bounceIn');
-                        });
-                    }
+            // var $object = $('.header');
+            // var position = $(this).position();
+            // var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+            // $object.scrollspy({
+            //     min: 0,
+            //     max: 200,
+            //     onEnter: function(element, position) {
+            //         console.log('Logo');
+            //         if(!$object.hasClass('fadeIn')){
+            //             $object.addClass('hinge').one(animationEnd, function() {
+            //                 $(this).removeClass('bounceIn');
+            //             });
+            //         }
 
-                },
-                onLeave: function(element, position) {
-                    console.log('Leaving');
-                    $object.removeClass('hinge').one(animationEnd, function() {
-                        $(this).addClass('fadeIn');
-                    });
+            //     },
+            //     onLeave: function(element, position) {
+            //         console.log('Leaving');
+            //         $object.removeClass('hinge').one(animationEnd, function() {
+            //             $(this).addClass('fadeIn');
+            //         });
 
-                },
-                onView: function(element, position) {
-                    $object.addClass('fadeIn');
-                }
-            });
+            //     },
+            //     onView: function(element, position) {
+            //         $object.addClass('fadeIn');
+            //     }
+            // });
 
-            var $content = $('.content');
-            var position = $content.position();
+            // var $content = $('.content');
+            // var position = $content.position();
 
-            $content.scrollspy({
-                min: $content.position.top,
-                max: $content.position.bottom,
-                onEnter: function(element, position) {
-                    $content.addClass('slideIn').one(animationEnd, function() {
-                        $(this).removeClass('slideIn').one(animationEnd, function() {
-                            $(this).addClass('bounceIn');
-                        });
-                    });
-                }
-            });
+            // $content.scrollspy({
+            //     min: $content.position.top,
+            //     max: $content.position.bottom,
+            //     onEnter: function(element, position) {
+            //         $content.addClass('slideIn').one(animationEnd, function() {
+            //             $(this).removeClass('slideIn').one(animationEnd, function() {
+            //                 $(this).addClass('bounceIn');
+            //             });
+            //         });
+            //     }
+            // });
 
         });
 });
