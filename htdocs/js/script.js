61593db716a0f3d5628c65acbcaede28375d8866
Dave Sayer <dave@bathdesign.co.uk>
2016-05-24 11:15:19 +0100

    tweaks to copy and styles

--- htdocs/js/script.js
+++ htdocs/js/script.js
@@ -3,7 +3,49 @@
  */
 jQuery( document ).ready( function($) {
 
-    $('.brand').hover(function(){
-        $(this).addClass('animated pulse');
-    });
+    $(function() {
+
+            var $object = $('.header');
+            var position = $(this).position();
+            var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+            $object.scrollspy({
+                min: 0,
+                max: 200,
+                onEnter: function(element, position) {
+                    console.log('Logo');
+                    if(!$object.hasClass('fadeIn')){
+                        $object.addClass('hinge').one(animationEnd, function() {
+                            $(this).removeClass('bounceIn');
+                        });
+                    }
+
+                },
+                onLeave: function(element, position) {
+                    console.log('Leaving');
+                    $object.removeClass('hinge').one(animationEnd, function() {
+                        $(this).addClass('fadeIn');
+                    });
+
+                },
+                onView: function(element, position) {
+                    $object.addClass('fadeIn');
+                }
+            });
+
+            var $content = $('.content');
+            var position = $content.position();
+
+            $content.scrollspy({
+                min: $content.position.top,
+                max: $content.position.bottom,
+                onEnter: function(element, position) {
+                    $content.addClass('slideIn').one(animationEnd, function() {
+                        $(this).removeClass('slideIn').one(animationEnd, function() {
+                            $(this).addClass('bounceIn');
+                        });
+                    });
+                }
+            });
+
+        });
 });
